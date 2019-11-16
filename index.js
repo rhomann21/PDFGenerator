970c54979074c06567de365918c3684952930dab
Rachael Homann <rachaelhomann@gmail.com>
2019-11-16 17:53:46 -0600

    pdf function added, stylized

--- index.js
+++ index.js
@@ -18,24 +18,27 @@ inquirer
       const bio = res.data.bio;
       const repos = res.data.public_repos;
       const blog = res.data.blog;
-
+      const repoName = res.data.login;
       return htmlStr = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
-</head>
-<body>
-      <div>${name}</div>
-      <div>${pic}</div>
-      <div>${bio}</div>
-      <div>${repos}</div>
-      <div>${blog}</div>
-      </body>
-</html>
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" type="text/css" href="./style.css" />
+            <title>${repoName}</title>
+      </head>
+      <body>
+            <div class="contentWrapper">
+            <div class="name"><h1>${name}</h1></div>
+            <div><img src="${pic}" height="200" width="200"></div>
+            <div><h3>${bio}</h3></div>
+            <div><h3>${repos}</h3></div>
+            <div><h3>${blog}</h3></div>
+            </div>
+            </body>
+      </html>
 `;
     })
     .then(htmlStr => {
@@ -45,14 +48,13 @@ inquirer
     .then(() => {
       /* read the file from filesystem */
       /* convert to pdf */
-      const run = async () => {
+      const createPDF = async () => {
         const html5ToPDF = new HTML5ToPDF({
-          inputPath: path.join(__dirname, "./index.html"),
-          outputPath: path.join(__dirname, "repos.pdf"),
-          //include: [
-        //    path.join(__dirname, "./node_modules/frow/dist/frow.min.css"),
-        //    path.join(__dirname, "./styles.css")
-        //  ],
+          inputPath: path.join(__dirname, `./index.html`),
+          outputPath: path.join(__dirname, `./${username}.pdf`),
+          include: [
+            path.join(__dirname, `./style.css`)
+          ],
           options: { printBackground: true }
         });
         await html5ToPDF.start();
@@ -61,6 +63,6 @@ inquirer
         console.log("DONE");
         process.exit(0);
       };
-      return run();
+    return { html: htmlStr, pdf: createPDF() }
     });
   });
