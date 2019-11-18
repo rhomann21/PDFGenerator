cbfb7c6ff1cd983000f16b92cf09f153b9bdd4ba
Rachael Homann <rachaelhomann@gmail.com>
2019-11-17 19:37:57 -0600

    location and github link added

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const HTML5ToPDF = require("html5-to-pdf");
 const path = require("path");
 let starCount = 0;
 inquirer
-  .prompt({
+  .prompt([{
     message: "Enter your GitHub username:",
     name: "username",
-  })
-  .then(function({ username }) {
+  },
+  {
+    message: "What is your favorite color?",
+    name: "color",
+  }])
+  .then(function({ username, color }) {
     const queryUrl = `https://api.github.com/users/${username}`;
 
     axios.get(queryUrl).then(res => {
@@ -19,7 +23,10 @@ inquirer
       const bio = res.data.bio;
       const repos = res.data.public_repos;
       const blog = res.data.blog;
-      const repoName = res.data.login;
+      const followers = res.data.followers;
+      const following = res.data.following;
+      const location = res.data.location;
+      const repoName = res.data.html_url;
       const qStarredURL = `https://api.github.com/users/${username}/repos`;
 
 
@@ -37,15 +44,20 @@ inquirer
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
                 <link rel="stylesheet" type="text/css" href="./style.css" />
-                <title>${repoName}</title>
+                <title>${name}</title>
           </head>
-          <body>
+          <body style="background-color:${color}">
                 <div class="contentWrapper">
                 <div class="name"><h1>${name}</h1></div>
                 <div><img src="${pic}" height="200" width="200"></div>
-                <div><h3>${bio}</h3></div>
-                <div><h3>${repos}</h3></div>
-                <div><h3>${blog}</h3></div>
+                <div><h3><a href src="${repoName}">GitHub Link</a></h3></div>
+                <div><h3>Located in ${location}</h3></div>
+                <div><h3>Bio: ${bio}</h3></div>
+                <div><h3>Number of public repos: ${repos}</h3></div>
+                <div><h3>Blog: ${blog}</h3></div>
+                <div><h3>Number of followers: ${followers}</h3></div>
+                <div><h3>Number of following: ${following}</h3></div>
+                <div><h3>Starred repos: ${starCount}</h3></div>
                 </div>
                 </body>
           </html>`
